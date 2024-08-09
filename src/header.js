77f34d3e3b8109c94fc181f3d422b6559933b716
mantequilla45 <vinzkenz45@gmail.com>
2024-08-09 14:36:03 +0800

    update

--- src/header.js
+++ src/header.js
@@ -27,7 +27,7 @@ function Header() {
                 className="logo" 
                 loading="lazy" 
                 href="/"
-                src="https://uc4a4d310b04d0a65908f02de03f.previews.dropboxusercontent.com/p/thumb/ACXCvLTW0s5Cm0FZDm-jOfncRLMW72LoBjjwK7CQ4tjK1q_TAx7STh4HGvQMbMIemltttjdlw9GmDSIT5NvVG8v-omy1FqXPNbv2rRvyQyJemZC9NR63R7NtiOtELqHkkSaug9F11pX8yJIJW1TGnKEpTZm-LIN0Zdja8OAjTy40Y8GtertTXcog4OQJmigE8Nyig6VVOXnTZp88l4PcHi-7M0IcWHLgsWFAmNRi4aoZ-R5xhx9LgsLhayCRpXFpQg6NG657MOkkgsKlnvk1DJtSLB60CmNSeM0TQbUhuLdVRpBh4IIv44S0mgBqWvYH0MWL8enXPTNjQUuNYLj2TBoSOANLoZl-JEe0HYBJoc0NHxaNMTXN1yTOm5ZrztutiVE/p.png?is_prewarmed=true" 
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/2c9f7d39735176760c056fc91eaf632fe1efec4d3171d173231809f32f114f8a?apiKey=0cd5b3eb85e74a83a268d41d07a9c27f&&apiKey=0cd5b3eb85e74a83a268d41d07a9c27f" 
                 alt="Buildit Logo" 
             />
         </div>
@@ -50,7 +50,7 @@ function Header() {
                         className="logo" 
                         loading="lazy" 
                         href="/"
-                        src="https://uc4a4d310b04d0a65908f02de03f.previews.dropboxusercontent.com/p/thumb/ACXCvLTW0s5Cm0FZDm-jOfncRLMW72LoBjjwK7CQ4tjK1q_TAx7STh4HGvQMbMIemltttjdlw9GmDSIT5NvVG8v-omy1FqXPNbv2rRvyQyJemZC9NR63R7NtiOtELqHkkSaug9F11pX8yJIJW1TGnKEpTZm-LIN0Zdja8OAjTy40Y8GtertTXcog4OQJmigE8Nyig6VVOXnTZp88l4PcHi-7M0IcWHLgsWFAmNRi4aoZ-R5xhx9LgsLhayCRpXFpQg6NG657MOkkgsKlnvk1DJtSLB60CmNSeM0TQbUhuLdVRpBh4IIv44S0mgBqWvYH0MWL8enXPTNjQUuNYLj2TBoSOANLoZl-JEe0HYBJoc0NHxaNMTXN1yTOm5ZrztutiVE/p.png?is_prewarmed=true" 
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/2c9f7d39735176760c056fc91eaf632fe1efec4d3171d173231809f32f114f8a?apiKey=0cd5b3eb85e74a83a268d41d07a9c27f&&apiKey=0cd5b3eb85e74a83a268d41d07a9c27f" 
                         alt="Buildit Logo" 
                     />
                 </div>
